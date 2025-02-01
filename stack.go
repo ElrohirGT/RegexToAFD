3c2f49e7bf798207e221afdc8e7c950786fa59d0
ElrohirGT <elrohirgt@gmail.com>
2025-02-01 15:19:48 -0600

    feat: Added stack implementation

---
+++ stack.go
@@ -0,0 +1,18 @@
+package main
+
+type Stack[T any] []T
+
+func (self *Stack[T]) Push(val T) *Stack[T] {
+	*self = append(*self, val)
+	return self
+}
+
+func (self *Stack[T]) Pop() T {
+	length := len(*self)
+
+	ref := *self
+	val := ref[length-1]
+	*self = ref[:length-1]
+
+	return val
+}
---
+++ stack_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestStack(t *testing.T) {
+	stack := Stack[int]{}
+
+	stack.
+		Push(50).
+		Push(100).
+		Push(150)
+
+	if val := stack.Pop(); val != 150 {
+		t.Fatalf("150 != %d", val)
+	}
+	if val := stack.Pop(); val != 100 {
+		t.Fatalf("100 != %d", val)
+	}
+	if val := stack.Pop(); val != 50 {
+		t.Fatalf("50 != %d", val)
+	}
+}
