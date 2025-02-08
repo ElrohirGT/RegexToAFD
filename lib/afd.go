98d230daacdd0ed65d3a64ab7de1120e412454c6
ElrohirGT <elrohirgt@gmail.com>
2025-02-08 16:00:02 -0600

    feat: updated afd lib

--- lib/afd.go
+++ lib/afd.go
@@ -1,5 +1,7 @@
 package lib
 
+import "fmt"
+
 type AFDState = string
 type AlphabetInput = string
 
@@ -16,4 +18,136 @@ type AFD struct {
 	// and the value is the new State the automata should transition.
 	Transitions      map[AFDState]map[AlphabetInput]AFDState
 	AcceptanceStates Set[AFDState]
+}
+
+type AFDPairType int
+
+func (self *AFD) IsAccepted(state *AFDState) bool {
+	_, found := self.AcceptanceStates[*state]
+	return found
+}
+
+func (self *AFD) MarkIfDistinguishable(aState *AFDState, bState *AFDState, table *AFDStateTable[AFDPairType]) AFDPairType {
+	afd := *self
+
+	if pairType, found := table.Get(aState, bState); found {
+		return pairType
+	}
+
+	aTransitions := afd.Transitions[*aState]
+	bTransitions := afd.Transitions[*bState]
+
+	if len(aTransitions) != len(bTransitions) {
+		msg := fmt.Sprintf("Supplied AFD is not really an AFD! The transitions length of these states didn't match\nAFD: %#v\nState A:%s\nState B: %s", afd, *aState, *bState)
+		panic(msg)
+	}
+
+	if *aState == *bState {
+		return EQUIVALENT
+	}
+
+	if afd.IsAccepted(aState) && !afd.IsAccepted(bState) ||
+		afd.IsAccepted(bState) && !afd.IsAccepted(aState) {
+		table.AddOrUpdate(*aState, *bState, DISTINCT)
+		return DISTINCT
+	}
+
+	for input, aOutState := range aTransitions {
+		bOutState, foundbOutState := bTransitions[input]
+		if !foundbOutState {
+			msg := fmt.Sprintf("B state doesn't contains the same input transition as A state!\nState B: %s\nState A: %s\nAFD: %#v", *bState, *aState, afd)
+			panic(msg)
+		}
+
+		if DISTINCT == self.MarkIfDistinguishable(&aOutState, &bOutState, table) {
+			table.AddOrUpdate(*aState, *bState, DISTINCT)
+			return DISTINCT
+		}
+	}
+
+	if pairType, found := table.Get(aState, bState); found {
+		return pairType
+	} else {
+		table.AddOrUpdate(*aState, *bState, EQUIVALENT)
+		return EQUIVALENT
+	}
+}
+
+const (
+	DISTINCT AFDPairType = iota
+	EQUIVALENT
+)
+
+type AFDStateTable[T any] map[AFDState]map[AFDState]T
+
+func (self *AFDStateTable[T]) PairAlreadyExists(a *AFDState, b *AFDState) bool {
+	s := *self
+	_, topLevelAFound := s[*a]
+	_, topLevelBFound := s[*b]
+
+	if !topLevelAFound || !topLevelBFound {
+		return false
+	}
+
+	if topLevelAFound {
+		if _, bFound := s[*a][*b]; bFound {
+			return true
+		}
+	}
+
+	if topLevelBFound {
+		if _, aFound := s[*b][*a]; aFound {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (self *AFDStateTable[T]) AddIfNotExists(a AFDState, b AFDState, stateType T) {
+	s := *self
+
+	_, aFound := s[a]
+	if !aFound {
+		s[a] = make(map[AFDState]T)
+	}
+
+	_, bFound := s[b]
+	if !bFound {
+		s[a][b] = stateType
+	}
+
+	_, bFound = s[b]
+	if !bFound {
+		s[b] = make(map[AFDState]T)
+	}
+
+	_, aFound = s[a]
+	if !aFound {
+		s[b][a] = stateType
+	}
+}
+
+func (self *AFDStateTable[T]) AddOrUpdate(a AFDState, b AFDState, stateType T) {
+	s := *self
+	s[a][b] = stateType
+	s[b][a] = stateType
+}
+
+func (self *AFDStateTable[T]) Get(a *AFDState, b *AFDState) (T, bool) {
+	var defaultPairType T
+
+	if !self.PairAlreadyExists(a, b) {
+		return defaultPairType, false
+	}
+
+	if pairType, found := (*self)[*a][*b]; found {
+		return pairType, true
+	}
+
+	if pairType, found := (*self)[*b][*a]; found {
+		return pairType, true
+	}
+
+	return defaultPairType, false
 }
