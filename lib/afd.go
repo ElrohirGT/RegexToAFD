064079d1d8676e38123bb89d4fb040096d4ac74c
ElrohirGT <elrohirgt@gmail.com>
2025-02-08 13:06:42 -0600

    feat: Improved type handling

--- lib/afd.go
+++ lib/afd.go
@@ -1,12 +1,19 @@
 package lib
 
+type AFDState = string
+type AlphabetInput = string
+
 type TransitionInput struct {
-	state string
-	input string
+	State AFDState
+	Input AlphabetInput
 }
 
 type AFD struct {
-	initialState     string
-	transitions      map[TransitionInput]string
-	acceptanceStates []string
+	InitialState AFDState
+	// A dictionary that contains a bunch of states.
+	// Each AFD state has another dictionary associated with it.
+	// Each key on this second dictionary represents an input from the alphabet,
+	// and the value is the new State the automata should transition.
+	Transitions      map[AFDState]map[AlphabetInput]AFDState
+	AcceptanceStates []AFDState
 }
