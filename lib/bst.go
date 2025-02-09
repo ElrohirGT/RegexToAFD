5fd2f086bf806c70431da4652a9f0a1e49233106
Jose-Prince <joseprince148@gmail.com>
2025-02-08 22:04:43 -0600

    Merge conflict

--- lib/bst.go
+++ lib/bst.go
@@ -11,6 +11,14 @@ type BST struct {
 	root *BSTNode
 }
 
+type TableRow struct {
+    nullable bool
+    firtspos []int
+    lastpos []int
+    followpos []int
+    simbol *rune
+}
+
 func (b *BST) Insert(n *BSTNode) {
 	b.root = b.root.insert(n)
 }
@@ -22,32 +30,43 @@ func (b *BSTNode) insert(n *BSTNode) *BSTNode {
 	if n.Val.value == nil {
 		b.left = b.left.insert(n)
 	} else {
-		// FIXME: Usa ZERO_OR_MANY :v
-		// if b.right == nil && (*b.Val.operator != ZERO_OR_MANY && *b.Val.operator != ONE_OR_MANY) {
-		//     b.right = b.right.insert(n)
-		// } else {
-		//     b.left = b.left.insert(n)
-		// }
+		if b.right == nil && *b.Val.operator != ZERO_OR_MANY {
+		    b.right = b.right.insert(n)
+		} else {
+		    b.left = b.left.insert(n)
+		}
+            
 	}
 	return b
 }
 
 func (b *BST) List() []*BSTNode {
-	ret := []*BSTNode{}
-	stack := []*BSTNode{}
-	current := b.root
-	for len(stack) != 0 || current != nil {
-		if current != nil {
-			stack = append(stack, current)
-			current = current.left
-		} else {
-			current = stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
-			ret = append(ret, current)
-			current = current.right
-		}
-	}
-	return ret
+    if b.root == nil {
+        return nil
+    }
+
+    result := []*BSTNode{}
+    stack := []*BSTNode{b.root}
+
+    for len(stack) > 0 {
+        current := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+
+        result = append(result, current)
+
+        if current.left != nil {
+            stack = append(stack, current.left)
+        }
+        if current.right != nil {
+            stack = append(stack, current.right)
+        }
+    }
+
+    for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+        result[i], result[j] = result[j], result[i]
+    }
+
+    return result
 }
 
 func (b *BST) insertion(postfix []RX_Token) {
@@ -56,3 +75,82 @@ func (b *BST) insertion(postfix []RX_Token) {
 		b.Insert(node)
 	}
 }
+
+func convertTreeToTable(nodes []*BSTNode) []*TableRow {
+    table := []*TableRow{}
+
+    // sets Leaf i first
+    for i, v := range nodes {
+        newRow := new(TableRow)
+
+        if v.Val.value != nil {
+            // nullable
+            newRow.nullable = false
+
+            // firstpos
+            newRow.firtspos = append(newRow.firtspos, i)
+
+            // lastpos
+            newRow.lastpos = append(newRow.lastpos, i)
+            
+        } else if *v.Val.operator == AND {
+            //nullable
+            newRow.nullable = table[i-2].nullable == true && newRow.nullable == true
+
+            // firstpos
+            if table[i-2].nullable == true {
+                union_slice := append(table[i-2].firtspos, table[i-1].firtspos ...)
+                newRow.firtspos = append(newRow.firtspos, union_slice ...)
+            } else {
+                newRow.firtspos = append(newRow.firtspos, table[i-2].firtspos ...)
+            }
+
+            // lastpos
+            if table[i-1].nullable == true {
+                union_slice := append(table[i-2].lastpos, table[i-1].lastpos ...)
+                newRow.lastpos = append(newRow.lastpos, union_slice ...)
+            } else {
+                newRow.lastpos = append(newRow.lastpos, table[i-1].lastpos ...)
+            }
+
+            // followpos
+            for _, pos := range table[i-2].lastpos {
+                table[pos].followpos = append(table[pos].followpos, table[i-1].firtspos ...)
+            }
+ 
+        } else if *v.Val.operator == OR {
+            // nullable
+            newRow.nullable = table[i-2].nullable == true || newRow.nullable == true 
+            
+            // firtspos
+            union_slice := append(table[i-2].firtspos, table[i-1].firtspos ...)
+            newRow.firtspos = append(newRow.firtspos, union_slice ...)
+
+            // lastpos
+            union_slice = append(table[i-2].lastpos, table[i-1].lastpos ...)
+            newRow.lastpos = append(newRow.lastpos, union_slice ...)
+
+        } else {
+            // nullable
+            newRow.nullable = true
+
+            // firstpos
+            newRow.firtspos = append(newRow.firtspos, table[i-1].firtspos ...)
+
+            // lastpos
+            newRow.lastpos = append(newRow.lastpos, table[i-1].lastpos ...)
+
+
+            // followpos
+            for _, pos := range newRow.lastpos {
+                table[pos].followpos = append(table[pos].followpos, newRow.firtspos ...)
+            }
+        }
+
+        table = append(table, newRow)
+    }
+
+    return table
+}
+
+
--- lib/bst_test.go
+++ lib/bst_test.go
@@ -25,8 +25,8 @@ func TestBST(t *testing.T) {
     got := tree.List()
 
     // Expected nodes 
-    expectedKeys := []int{3,1,2}
-    expectedVals := []RX_Token{CreateValueToken('b'),CreateOperatorToken(AND),CreateValueToken('a')}
+    expectedKeys := []int{3,2,1}
+    expectedVals := []RX_Token{CreateValueToken('b'),CreateValueToken('a'),CreateOperatorToken(AND)}
 
     // Verifies total nodes
     if len(got) != len(expectedKeys) {
@@ -86,13 +86,12 @@ func TestExampleBST(t *testing.T) {
     got := tree.List()
 
     // Expected nodes 
-    expectedKeys := []int{11,9,10,8,6,7,4,5,2,3,0,1}
+    expectedKeys := []int{11,10,9,8,7,6,5,4,3,2,1,0}
     expectedVals := []RX_Token{
         CreateValueToken('b'),
-        CreateOperatorToken(OR),
         CreateValueToken('a'),
+        CreateOperatorToken(OR),
         CreateOperatorToken(ZERO_OR_MANY),
-        CreateOperatorToken(AND),
         CreateValueToken('a'),
         CreateOperatorToken(AND),
         CreateValueToken('b'),
@@ -100,6 +99,7 @@ func TestExampleBST(t *testing.T) {
         CreateValueToken('b'),
         CreateOperatorToken(AND),
         CreateValueToken('#'),
+        CreateOperatorToken(AND),
     }
 
     // Verifies total nodes
@@ -130,3 +130,106 @@ func TestExampleBST(t *testing.T) {
     }
 }
 
+func TestTable(t *testing.T) {
+    nodes := []*BSTNode{
+        {Key: 1, Val: CreateOperatorToken(AND)},
+        {Key: 2, Val: CreateValueToken('a')},
+        {Key: 3, Val: CreateValueToken('b')},
+    }
+
+    // Creates tree
+    tree := new(BST)
+
+    // Insertar nodos
+    for _, node := range nodes {
+        tree.Insert(node)
+    }
+
+    // in-order transverse
+    got := tree.List()
+
+    table := convertTreeToTable(got)
+
+	// Valores esperados
+	expectedFirstPos := [][]int{{0}, {1}, {0}}
+	expectedLastPos := [][]int{{0}, {1}, {1}}
+    expectedFollowPos := [][]int{{1},{},{}}
+	expectedNullable := []bool{false, false, false}
+
+	// Validar la tabla generada
+	for i, row := range table {
+		if !equalSlices(row.firtspos, expectedFirstPos[i]) {
+			t.Errorf("Error en firstpos en índice %d: esperado %v, obtenido %v", i, expectedFirstPos[i], row.firtspos)
+		}
+		if !equalSlices(row.lastpos, expectedLastPos[i]) {
+			t.Errorf("Error en lastpos en índice %d: esperado %v, obtenido %v", i, expectedLastPos[i], row.lastpos)
+		}
+        if !equalSlices(row.followpos, expectedFollowPos[i]) {
+			t.Errorf("Error en lastpos en índice %d: esperado %v, obtenido %v", i, expectedFollowPos[i], row.followpos)
+		}
+
+		if row.nullable != expectedNullable[i] {
+			t.Errorf("Error en nullable en índice %d: esperado %v, obtenido %v", i, expectedNullable[i], row.nullable)
+		}
+	}
+}
+
+func TestExampleTable(t *testing.T) {
+   nodes := []*BSTNode{
+        {Key: 0, Val: CreateOperatorToken(AND)},
+        {Key: 1, Val: CreateValueToken('#')},
+        {Key: 2, Val: CreateOperatorToken(AND)},
+        {Key: 3, Val: CreateValueToken('b')},
+        {Key: 4, Val: CreateOperatorToken(AND)},
+        {Key: 5, Val: CreateValueToken('b')},
+        {Key: 6, Val: CreateOperatorToken(AND)},
+        {Key: 7, Val: CreateValueToken('a')},
+        {Key: 8, Val: CreateOperatorToken(ZERO_OR_MANY)},
+        {Key: 9, Val: CreateOperatorToken(OR)},
+        {Key: 10, Val: CreateValueToken('a')},
+        {Key: 11, Val: CreateValueToken('b')},
+    }
+
+    // Creates tree
+    tree := new(BST)
+
+    // Insertar nodos
+    for _, node := range nodes {
+        tree.Insert(node)
+    }
+
+    // in-order transverse
+    got := tree.List()
+
+    table := convertTreeToTable(got)
+
+	// Valores esperados
+	expectedFirstPos := [][]int{{0}, {1}, {0,1},{0,1},{4},{0,1,4},{6},{0,1,4},{8},{0,1,4},{10},{0,1,4}}
+	expectedLastPos := [][]int{{0}, {1}, {0,1},{0,1},{4},{4},{6},{6},{8},{8},{10},{10}}
+	expectedNullable := []bool{false, false, false, true, false, false, false, false, false, false, false, false}
+
+	// Validar la tabla generada
+	for i, row := range table {
+		if !equalSlices(row.firtspos, expectedFirstPos[i]) {
+			t.Errorf("Error en firstpos en índice %d: esperado %v, obtenido %v", i, expectedFirstPos[i], row.firtspos)
+		}
+		if !equalSlices(row.lastpos, expectedLastPos[i]) {
+			t.Errorf("Error en lastpos en índice %d: esperado %v, obtenido %v", i, expectedLastPos[i], row.lastpos)
+		}
+		if row.nullable != expectedNullable[i] {
+			t.Errorf("Error en nullable en índice %d: esperado %v, obtenido %v", i, expectedNullable[i], row.nullable)
+		}
+	}
+}
+
+func equalSlices(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
